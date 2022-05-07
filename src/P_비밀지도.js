a79851c04f1924bb75437b1f526fdcd6b233c67f
starguys <kjj9500@gmail.com>
2022-05-07 16:15:21 +0900

    2022-05-07 codeing study

--- src/P_비밀지도.js
+++ src/P_비밀지도.js
@@ -1,19 +1,41 @@
 function solution(n, arr1, arr2) {
-  const index = Array.from(Array(n), () => Array(n).fill(0));
-  const answer = [n, arr1, arr2];
   const arr1Binary = arr1.map((el) => el.toString(2).split(""));
   const arr2Binary = arr2.map((el) => el.toString(2).split(""));
   for (let i = 0; i < arr1Binary.length; i++) {
-    //   arr1Binary[i].push(0);
-    for (let j = n; j < arr1Binary[i].length; j++) {
-      console.log(n, j);
+    for (let j = 0; j < arr1Binary[i].length; j++) {
+      if (arr1Binary[i].length < n) {
+        arr1Binary[i].unshift("0");
+      }
     }
-
-    console.log(arr1Binary[i].length);
   }
 
-  return null;
-  //   return [arr1Binary, arr2Binary];
+  for (let i = 0; i < arr2Binary.length; i++) {
+    for (let j = 0; j < arr2Binary[i].length; j++) {
+      if (arr2Binary[i].length < n) {
+        arr2Binary[i].unshift("0");
+      }
+    }
+  }
+
+  for (let i = 0; i < arr1Binary.length; i++) {
+    for (let j = 0; j < arr1Binary[i].length; j++) {
+      if (arr1Binary[i][j] !== "1") {
+        arr1Binary[i][j] = arr2Binary[i][j];
+      }
+    }
+  }
+
+  for (let i = 0; i < arr1Binary.length; i++) {
+    for (let j = 0; j < arr1Binary[i].length; j++) {
+      if (arr1Binary[i][j] === "1") {
+        arr1Binary[i][j] = "#";
+      } else {
+        arr1Binary[i][j] = " ";
+      }
+    }
+  }
+
+  return arr1Binary.map((el) => el.join(""));
 }
 
 console.log(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]));
