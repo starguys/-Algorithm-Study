071513ae8f00e7e657b806c86a015615976c36db
starguys <kjj9500@gmail.com>
2022-05-08 19:39:09 +0900

    2022-05-08 coding study

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("hi");
+function solution(n) {
+  let divN = Math.floor(n / 2);
+  let result = 1;
+  let Num = 0;
+  for (let i = 1; i < divN + 1; i++) {
+    Num = i;
+    for (let j = i + 1; Num < n + 1; j++) {
+      console.log(j, "j", i, "i");
+      Num = Num + j;
+      console.log(Num, "Num");
+      if (Num === n) {
+        result++;
+        console.log(result, "result");
+      }
+    }
+  }
+  return result;
+}
+
+console.log(solution(15));
+
+//4
+//22 4
+
+//15
+//12345
+//
+
+//7+8 T
+//6+7+8 F
+//5+6+7 F
+//4+5+6 T
+//3+4+5+6 F
+//2+3+4+5+6 F
+//1+2+3+4+5 T
+
+//16
+//1+2+3+4+5+6
+
+//시간초과
---
+++ index2.js
@@ -0,0 +1,8 @@
+function solution(expression) {
+  console.log(splitMulti(expression, ["+", "-"]));
+  var answer = 0;
+  return answer;
+}
+
+console.log(solution("100-200*300-500+20"));
+// console.log(solution("50*6-3*2"));
